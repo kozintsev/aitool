e81333caf103c6be66ea3333a784b0d6429a2af3
O.Kozintsev <O.Kozintsev@4019c624-236c-11df-b1bd-79c33a1377a4>
2010-03-31 14:18:37 +0000

    Plugin для для работы с Kompas 3D

---
+++ Kompas3Dplugin/Kompas3Dplugin/Kompas3Dplugin/Kompas3Dplugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Kompas3Dplugin
+{
+    public class Kompas3D
+    {
+    }
+}
