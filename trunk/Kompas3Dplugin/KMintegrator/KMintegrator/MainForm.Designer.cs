36594c4f354d395174bb37a479d66b7b68a69ce3
O.Kozintsev <O.Kozintsev@4019c624-236c-11df-b1bd-79c33a1377a4>
2010-04-15 07:32:49 +0000

    Разработка дизайна

--- trunk/Kompas3Dplugin/KMintegrator/KMintegrator/MainForm.Designer.cs
+++ trunk/Kompas3Dplugin/KMintegrator/KMintegrator/MainForm.Designer.cs
@@ -28,20 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NameKompas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KompasValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameMath = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.MathName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KompasParam = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.KompasPath = new System.Windows.Forms.TextBox();
+            this.MathPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AddKompas = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AddMathCad = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameKompas,
+            this.KompasValue,
+            this.NameMath});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 169);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // NameKompas
+            // 
+            this.NameKompas.HeaderText = "KompasName";
+            this.NameKompas.Name = "NameKompas";
+            // 
+            // KompasValue
+            // 
+            this.KompasValue.HeaderText = "KompasValue";
+            this.KompasValue.Name = "KompasValue";
+            this.KompasValue.ReadOnly = true;
+            // 
+            // NameMath
+            // 
+            this.NameMath.HeaderText = "MathName";
+            this.NameMath.Name = "NameMath";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Передача переменных их MAthCAD в Kompas";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MathName,
+            this.Value,
+            this.KompasParam});
+            this.dataGridView2.Location = new System.Drawing.Point(21, 243);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(432, 168);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // MathName
+            // 
+            this.MathName.HeaderText = "MathName";
+            this.MathName.Name = "MathName";
+            this.MathName.ReadOnly = true;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // KompasParam
+            // 
+            this.KompasParam.HeaderText = "KompasParam";
+            this.KompasParam.Name = "KompasParam";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Передача переменных из Kompas в MathCAD";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // KompasPath
+            // 
+            this.KompasPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.KompasPath.Location = new System.Drawing.Point(21, 475);
+            this.KompasPath.Name = "KompasPath";
+            this.KompasPath.Size = new System.Drawing.Size(351, 20);
+            this.KompasPath.TabIndex = 4;
+            // 
+            // MathPath
+            // 
+            this.MathPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.MathPath.Location = new System.Drawing.Point(21, 523);
+            this.MathPath.Name = "MathPath";
+            this.MathPath.Size = new System.Drawing.Size(351, 20);
+            this.MathPath.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Путь к файлу Kompas:";
+            // 
+            // AddKompas
+            // 
+            this.AddKompas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddKompas.Location = new System.Drawing.Point(378, 475);
+            this.AddKompas.Name = "AddKompas";
+            this.AddKompas.Size = new System.Drawing.Size(75, 23);
+            this.AddKompas.TabIndex = 7;
+            this.AddKompas.Text = "Добавить";
+            this.AddKompas.UseVisualStyleBackColor = true;
+            this.AddKompas.Click += new System.EventHandler(this.AddKompas_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 507);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Путь к файлу MathCAD";
+            // 
+            // AddMathCad
+            // 
+            this.AddMathCad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddMathCad.Location = new System.Drawing.Point(378, 521);
+            this.AddMathCad.Name = "AddMathCad";
+            this.AddMathCad.Size = new System.Drawing.Size(75, 23);
+            this.AddMathCad.TabIndex = 9;
+            this.AddMathCad.Text = "Добавить";
+            this.AddMathCad.UseVisualStyleBackColor = true;
+            this.AddMathCad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Применить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 566);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(487, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(117, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Обновить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(117, 417);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Обновить";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 480);
+            this.ClientSize = new System.Drawing.Size(487, 588);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AddMathCad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AddKompas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MathPath);
+            this.Controls.Add(this.KompasPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.MinimumSize = new System.Drawing.Size(495, 615);
             this.Name = "MainForm";
             this.Text = "Kompas-MathCAD Integrator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameKompas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KompasValue;
+        private System.Windows.Forms.DataGridViewComboBoxColumn NameMath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MathName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewComboBoxColumn KompasParam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox KompasPath;
+        private System.Windows.Forms.TextBox MathPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button AddKompas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button AddMathCad;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- trunk/Kompas3Dplugin/KMintegrator/KMintegrator/MainForm.cs
+++ trunk/Kompas3Dplugin/KMintegrator/KMintegrator/MainForm.cs
@@ -15,5 +15,35 @@ namespace KMintegrator
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddKompas_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            //txt files (*.txt)|*.txt|All files (*.*)|*.*
+            dlg.Filter = "КОМПАС-Документы (*.cdw; *.m3d;*.a3d)|*.cdw;*.m3d;*.a3d";
+            dlg.Filter += "|КОМПАС-Чертёж (*.cdw)|*.cdw";
+            dlg.Filter += "|КОМПАС-Деталь (*.m3d)|*.m3d";
+            dlg.Filter += "|КОМПАС-Сборка (*.a3d)|*.a3d";
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+            KompasPath.Text = dlg.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Файлы MathCAD 14 (*.xmcd)|*.xmcd";
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+            MathPath.Text = dlg.FileName;
+
+        }
     }
 }
