920f295bf16c614fe8214dd4c4b66fb61a49cd08
O.Kozintsev <O.Kozintsev@4019c624-236c-11df-b1bd-79c33a1377a4>
2010-04-23 11:00:15 +0000

    Изменение структуры

---
+++ AiToolGui/AiToolGui/DocumentManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AiToolGui
+{
+    public partial class DocumentManager : Form
+    {
+        public DocumentManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AiToolGui/AiToolGui/ProjectManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AiToolGui
+{
+    public partial class ProjectManager : Form
+    {
+  
+        public ProjectManager()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectManager_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        void Button4Click(object sender, EventArgs e)
+        {
+        	Close();
+        }
+
+        private void ProjectManager_FormClosed(object sender, FormClosedEventArgs e)
+        {
+      
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(e.ToString());
+        }
+
+        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            //
+            MessageBox.Show(e.ToString());
+        }
+        
+        void ListView1ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+        	
+        }
+    }
+}
