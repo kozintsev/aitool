9bfd773b13dc8e9b5d4905c5698354bc9562132c
O.Kozintsev <O.Kozintsev@gmail.com>
2011-03-29 13:22:59 +0000

    добавленная ER модель БД со структурой спецификации, пока в коде есть наброски сохранение его в XML. Поправленная ER модель БД AiTool.

--- AiToolGui/AiToolGui/CreateSpecification.cs
+++ AiToolGui/AiToolGui/CreateSpecification.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace AiToolGui
 {
@@ -16,9 +17,77 @@ namespace AiToolGui
             InitializeComponent();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Save_Click(object sender, EventArgs e)
+        {
+            //cохранение пока в XML
+            if (textBox1.Text.Length < 1) 
+            {
+                MessageBox.Show("Слишком короткое Обозначение" );
+                return;
+            }
+            if (textBox2.Text.Length < 1)
+            {
+                MessageBox.Show("Слишком короткое Наименование");
+                return;
+            }
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "untitled";
+            dlg.DefaultExt = "xml";
+            dlg.Filter = "Файлы XML (*.xml)|*.xml";
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            // начинаем сохранять
+            XmlTextWriter writer = null;
+            try
+            {
+                // создаем класс для сохранения XML
+                writer = new XmlTextWriter(dlg.FileName, System.Text.Encoding.UTF8);
+                // форматирование, чтобы файл не был вытянут в одну линию
+                writer.Formatting = Formatting.Indented;
+
+                // пишем заголовок и корневой элемент
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Specification");
+                //аттрибуты элемента
+                //writer.WriteAttributeString("birth", person.Birth.ToShortDateString());
+                //writer.WriteAttributeString("death", person.Death.ToShortDateString());
+                //вложенный элемент
+                writer.WriteStartElement("description");
+                writer.WriteString(textBox1.Text);
+                writer.WriteEndElement();
+                // вложенный элемент
+                writer.WriteStartElement("name");
+                writer.WriteString(textBox2.Text);
+                writer.WriteEndElement();
+    
+                // закрываем корневой элемент и завершаем работу с документом
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!");
+            }
+            finally
+            {
+                // закрываем файл
+                if (writer != null) writer.Close();
+            }
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+    
     }
 }
--- AiToolGui/AiToolGui/Program.cs
+++ AiToolGui/AiToolGui/Program.cs
@@ -22,6 +22,7 @@ namespace AiToolGui
 		[STAThread]
 		private static void Main(string[] args)
 		{
+
             Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new AITool());
