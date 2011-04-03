79dc8eb22a18023ef560452ca5795a345ed4cc2b
o.kozintsev <o.kozintsev@4019c624-236c-11df-b1bd-79c33a1377a4>
2011-04-03 13:54:11 +0000

    [+]поле для ввода аннотации [+]ввод элементов характеристик

--- trunk/AiToolGui/AiToolGui/CreateSpecification.cs
+++ trunk/AiToolGui/AiToolGui/CreateSpecification.cs
@@ -17,6 +17,7 @@ namespace AiToolGui
             InitializeComponent();
         }
 
+
         private void Save_Click(object sender, EventArgs e)
         {
             //cохранение пока в XML
@@ -86,6 +87,83 @@ namespace AiToolGui
         private void Close_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private bool FindDouble(TreeNode node, string AddText)
+        {
+            
+            for (int i = 0; i < node.Nodes.Count; i++)
+            {
+                if (AddText == node.Nodes[i].Text)
+                {
+                    MessageBox.Show("Такой узел уже есть");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // добавить характеристику в дерево child
+            string AddText = AddNodeText.Text;
+            if (AddText == "")
+            {
+                MessageBox.Show("Введите текст");
+                return;
+            }
+            TreeNode node;
+            node = treeView1.SelectedNode;
+
+            for (int i = 0; i < node.Nodes.Count; i++)
+            {
+                if (AddText == node.Nodes[i].Text)
+                {
+                    MessageBox.Show("Такой узел уже есть");
+                    return;
+                }
+            }
+    
+            
+            
+            node.Nodes.Add(AddNodeText.Text);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // добавить root
+            string AddText = AddNodeText.Text;
+            if (AddText == "")
+            {
+                MessageBox.Show("Введите текст");
+                return;
+            }
+
+            //if (FindDouble(treeView1, AddNodeText.Text)) return;
+            
+            for (int i = 0; i < treeView1.Nodes.Count; i++)
+            {
+                if (AddText == treeView1.Nodes[i].Text)
+                {
+                    MessageBox.Show("Такой узел уже есть");
+                    return;
+                }
+            }
+             
+                //treeView1.Nodes.Count
+            treeView1.Nodes.Add(AddText);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_ParentChanged(object sender, EventArgs e)
+        {
+                       
         }
 
     
