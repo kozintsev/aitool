d421f9e699bfa82ee767797a88052965732a9af3
O.Kozintsev <O.Kozintsev@googlemail.com>
2011-08-30 10:15:46 +0000

    Добавление узлов в конфигуратор, осталось добавить в БД

--- AiToolGui/AiToolGui/ClassfierForm.Designer.cs
+++ AiToolGui/AiToolGui/ClassfierForm.Designer.cs
@@ -28,18 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClassfierForm));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textNode = new System.Windows.Forms.TextBox();
+            this.toolTree = new System.Windows.Forms.ToolStrip();
+            this.tAddTree = new System.Windows.Forms.ToolStripButton();
+            this.tAddNode = new System.Windows.Forms.ToolStripButton();
+            this.tDelNode = new System.Windows.Forms.ToolStripButton();
             this.treeClassfier = new System.Windows.Forms.TreeView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.toolRich = new System.Windows.Forms.ToolStrip();
             this.buttonExit = new System.Windows.Forms.Button();
             this.buttonOpen = new System.Windows.Forms.Button();
             this.richBox = new System.Windows.Forms.RichTextBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
-            this.toolTree = new System.Windows.Forms.ToolStrip();
-            this.toolRich = new System.Windows.Forms.ToolStrip();
-            this.buttonSave = new System.Windows.Forms.Button();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.toolTree.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -50,6 +56,7 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.textNode);
             this.splitContainer1.Panel1.Controls.Add(this.toolTree);
             this.splitContainer1.Panel1.Controls.Add(this.treeClassfier);
             // 
@@ -66,15 +73,84 @@
             this.splitContainer1.SplitterWidth = 2;
             this.splitContainer1.TabIndex = 0;
             // 
+            // textNode
+            // 
+            this.textNode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textNode.Location = new System.Drawing.Point(3, 28);
+            this.textNode.Name = "textNode";
+            this.textNode.Size = new System.Drawing.Size(196, 20);
+            this.textNode.TabIndex = 2;
+            // 
+            // toolTree
+            // 
+            this.toolTree.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tAddTree,
+            this.tAddNode,
+            this.tDelNode});
+            this.toolTree.Location = new System.Drawing.Point(0, 0);
+            this.toolTree.Name = "toolTree";
+            this.toolTree.Size = new System.Drawing.Size(203, 25);
+            this.toolTree.TabIndex = 1;
+            this.toolTree.Text = "toolStrip1";
+            // 
+            // tAddTree
+            // 
+            this.tAddTree.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tAddTree.Image = ((System.Drawing.Image)(resources.GetObject("tAddTree.Image")));
+            this.tAddTree.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tAddTree.Name = "tAddTree";
+            this.tAddTree.Size = new System.Drawing.Size(23, 22);
+            this.tAddTree.Text = "Add Root";
+            this.tAddTree.Click += new System.EventHandler(this.tAddTree_Click);
+            // 
+            // tAddNode
+            // 
+            this.tAddNode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tAddNode.Image = ((System.Drawing.Image)(resources.GetObject("tAddNode.Image")));
+            this.tAddNode.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tAddNode.Name = "tAddNode";
+            this.tAddNode.Size = new System.Drawing.Size(23, 22);
+            this.tAddNode.Text = "Add Node";
+            this.tAddNode.Click += new System.EventHandler(this.tAddNode_Click);
+            // 
+            // tDelNode
+            // 
+            this.tDelNode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tDelNode.Image = ((System.Drawing.Image)(resources.GetObject("tDelNode.Image")));
+            this.tDelNode.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tDelNode.Name = "tDelNode";
+            this.tDelNode.Size = new System.Drawing.Size(23, 22);
+            this.tDelNode.Text = "Delete Node";
+            this.tDelNode.Click += new System.EventHandler(this.tDelNode_Click);
+            // 
             // treeClassfier
             // 
             this.treeClassfier.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.treeClassfier.Location = new System.Drawing.Point(0, 0);
+            this.treeClassfier.Location = new System.Drawing.Point(0, 54);
             this.treeClassfier.Name = "treeClassfier";
-            this.treeClassfier.Size = new System.Drawing.Size(200, 474);
+            this.treeClassfier.Size = new System.Drawing.Size(200, 420);
             this.treeClassfier.TabIndex = 0;
+            this.treeClassfier.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.SelectNode);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(113, 427);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 5;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // toolRich
+            // 
+            this.toolRich.Location = new System.Drawing.Point(3, 0);
+            this.toolRich.Name = "toolRich";
+            this.toolRich.Size = new System.Drawing.Size(373, 25);
+            this.toolRich.TabIndex = 4;
+            this.toolRich.Text = "toolStrip1";
             // 
             // buttonExit
             // 
@@ -116,31 +192,6 @@
             this.splitter1.TabIndex = 0;
             this.splitter1.TabStop = false;
             // 
-            // toolTree
-            // 
-            this.toolTree.Location = new System.Drawing.Point(0, 0);
-            this.toolTree.Name = "toolTree";
-            this.toolTree.Size = new System.Drawing.Size(203, 25);
-            this.toolTree.TabIndex = 1;
-            this.toolTree.Text = "toolStrip1";
-            // 
-            // toolRich
-            // 
-            this.toolRich.Location = new System.Drawing.Point(3, 0);
-            this.toolRich.Name = "toolRich";
-            this.toolRich.Size = new System.Drawing.Size(373, 25);
-            this.toolRich.TabIndex = 4;
-            this.toolRich.Text = "toolStrip1";
-            // 
-            // buttonSave
-            // 
-            this.buttonSave.Location = new System.Drawing.Point(113, 427);
-            this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
-            this.buttonSave.TabIndex = 5;
-            this.buttonSave.Text = "Save";
-            this.buttonSave.UseVisualStyleBackColor = true;
-            // 
             // ClassfierForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -154,6 +205,8 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
             this.splitContainer1.ResumeLayout(false);
+            this.toolTree.ResumeLayout(false);
+            this.toolTree.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -169,5 +222,9 @@
         private System.Windows.Forms.ToolStrip toolTree;
         private System.Windows.Forms.ToolStrip toolRich;
         private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textNode;
+        private System.Windows.Forms.ToolStripButton tAddTree;
+        private System.Windows.Forms.ToolStripButton tAddNode;
+        private System.Windows.Forms.ToolStripButton tDelNode;
     }
 }
--- AiToolGui/AiToolGui/ClassfierForm.cs
+++ AiToolGui/AiToolGui/ClassfierForm.cs
@@ -11,14 +11,74 @@ namespace AiToolGui
 {
     public partial class ClassfierForm : Form
     {
+        private Settings sett;
+        private ConnectDataBase cdb;
         public ClassfierForm()
         {
             InitializeComponent();
+            sett = new Settings();
+            cdb = new ConnectDataBase();
+            cdb.CreateConnectDataBase();
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void tAddTree_Click(object sender, EventArgs e)
+        {
+            string AddText = textNode.Text;
+            if (AddText == "")
+            {
+                MessageBox.Show("Введите текст");
+                return;
+            }
+
+            for (int i = 0; i < treeClassfier.Nodes.Count; i++)
+            {
+                if (AddText == treeClassfier.Nodes[i].Text)
+                {
+                    MessageBox.Show("Такой узел уже есть");
+                    return;
+                }
+            }
+            TreeNode node = new TreeNode();
+            node.Text = AddText;
+            treeClassfier.Nodes.Add(node);
+            textNode.Text = "";
+        }
+
+        private void tAddNode_Click(object sender, EventArgs e)
+        {
+            // добавить характеристику в дерево child
+            string AddText = textNode.Text;
+            if (AddText == "")
+            {
+                MessageBox.Show("Введите текст");
+                return;
+            }
+            TreeNode selnode;
+            selnode = treeClassfier.SelectedNode;
+            TreeNode node = new TreeNode();
+            node.Text = AddText;
+            selnode.Nodes.Add(node);
+            textNode.Text = "";
+        }
+
+        private void tDelNode_Click(object sender, EventArgs e)
+        {
+            TreeNode node;
+            node = treeClassfier.SelectedNode;
+            if (node == null) return;
+            node.Remove();
+        }
+
+        private void SelectNode(object sender, TreeViewEventArgs e)
+        {
+            TreeNode node;
+            node = e.Node;
+            textNode.Text = node.Text;
         }
     }
 }
