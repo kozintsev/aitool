eb8039666bf8179714d0c11ae955ab10e8a7c811
O.Kozintsev@gmail.com <O.Kozintsev@gmail.com@4019c624-236c-11df-b1bd-79c33a1377a4>
2012-02-21 07:37:41 +0000

    Добавил класс для проектных процедур.

---
+++ trunk/AiToolGui/AiToolGui/CProjectProcedures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AiToolGui
+{
+    public class CProjectProcedures
+    {
+        public int idProjectProcedure;
+        public string ProcedurName;
+        public string ProcedurDescription;
+    }
+}
